7f1c8e584f82f3a788564893469217278740e6ba
Kirti-Girdhar <kirtigirdhar2004@gmail.com>
2025-11-14 09:37:38 +0530

    Initial commit: add workspace files

---
+++ translator.py
@@ -0,0 +1,13 @@
+def translate(phrase):
+    translations = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translations= translations+'G'
+            else:
+                translations =translations+'g'
+        else:
+            translations=translations+letter
+    return translations
+
+print(translate(input("Enter a phrase: ")))
